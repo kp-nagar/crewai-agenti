8c5abe3b507efb0461a79fcce512d5d4c2fc72fb
Kumarpal Nagar <kumarpal.nagar@gmail.com>
2025-08-02 19:38:30 +0530

    add FastAPI endpoint.

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel
+from src.techai.crew import Techai
+
+class InputQuery(BaseModel):
+    query: str
+
+
+app = FastAPI()
+
+
+def run_crew_in_background(inputs: dict):
+    Techai().crew().kickoff(inputs=inputs)
+
+
+@app.post("/generate-question/")
+async def create_item(iqu: InputQuery, background_tasks: BackgroundTasks):
+    inputs = {
+        'topic': iqu.query,
+    }
+    background_tasks.add_task(run_crew_in_background, inputs)
+    return "done"
